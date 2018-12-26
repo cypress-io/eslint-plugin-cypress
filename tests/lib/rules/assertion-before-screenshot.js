97d75ff0e660805e7a232f5b553f7511f1700b38
Luke Page <luke.a.page@gmail.com>
2018-12-26 13:43:07 +0100

    Final fixes and test fixes

--- tests/lib/rules/assertion-before-screenshot.js
+++ tests/lib/rules/assertion-before-screenshot.js
@@ -11,13 +11,14 @@ const parserOptions = { ecmaVersion: 6 }
 ruleTester.run('assertion-before-screenshot', rule, {
   valid: [
     { code: 'cy.get(".some-element"); cy.screenshot();', parserOptions },
-    { code: 'cy.get(".some-element").screenshot();', parserOptions },
-    { code: 'cy.get(".some-element").should("be.visible"); cy.screenshot();', parserOptions },
-    { code: 'cy.get(".some-element").screenshot().click()', parserOptions, errors },
-    { code: 'cy.get(".some-element"); if(true) cy.screenshot();', parserOptions },
-    { code: 'if(true) { cy.get(".some-element"); cy.screenshot(); }', parserOptions },
-    { code: 'cy.get(".some-element"); if(true) { cy.screenshot(); }', parserOptions },
-    { code: 'const a = () => { cy.get(".some-element"); cy.screenshot(); }', parserOptions, errors },
+    { code: 'cy.get(".some-element").should("exist").screenshot();', parserOptions },
+    { code: 'cy.get(".some-element").should("exist").screenshot().click()', parserOptions, errors },
+    { code: 'cy.get(".some-element").should("exist"); if(true) cy.screenshot();', parserOptions },
+    { code: 'if(true) { cy.get(".some-element").should("exist"); cy.screenshot(); }', parserOptions },
+    { code: 'cy.get(".some-element").should("exist"); if(true) { cy.screenshot(); }', parserOptions },
+    { code: 'const a = () => { cy.get(".some-element").should("exist"); cy.screenshot(); }', parserOptions, errors },
+    { code: 'cy.get(".some-element").should("exist").and("be.visible"); cy.screenshot();', parserOptions },
+    { code: 'cy.get(".some-element").contains("Text"); cy.screenshot();', parserOptions },
   ],
 
   invalid: [
