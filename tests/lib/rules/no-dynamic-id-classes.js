37da9c566a417306f16f677b0003aa48db2660ae
sb <sbaldawa@slack-corp.com>
2019-09-17 17:52:52 -0700

    check for data attribute

--- tests/lib/rules/no-dynamic-id-classes.js
+++ tests/lib/rules/no-dynamic-id-classes.js
@@ -10,14 +10,16 @@ const parserOptions = { ecmaVersion: 6 }
 
 ruleTester.run('no-dynamic-id-classes', rule, {
   valid: [
-    { code: 'cy.get("data-qa")', parserOptions },
-    { code: 'cy.get("button").click()', parserOptions },
+    { code: 'cy.get(\'[data-cy=submit]\').click()', parserOptions },
+    { code: 'cy.get(\'[data-QA=submit]\')', parserOptions },
     { code: 'cy.clock(5000)', parserOptions },
     { code: 'cy.scrollTo(0, 10)', parserOptions },
     { code: 'cy.tick(500)', parserOptions },
   ],
 
   invalid: [
+    { code: 'cy.get(\'[daedta-cy=submit]\').click()', parserOptions, errors },
+    { code: 'cy.get(\'[d-cy=submit]\')', parserOptions, errors },
     { code: 'cy.get(".btn-large").click()', parserOptions, errors },
     { code: 'cy.get(".btn-.large").click()', parserOptions, errors },
     { code: 'cy.get(".a")', parserOptions, errors },
