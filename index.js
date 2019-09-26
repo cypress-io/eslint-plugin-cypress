a4a0e8e7b7028193193975676b5639e8124942c4
sandeepbaldawa <sandeepbaldawa@gmail.com>
2019-09-26 07:19:22 -0700

    feat(rule): add 'require-data-selectors' rule (#30)
    Only allow `[data-*]` attribute selectors on `cy.get`

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
     'no-assigning-return-values': require('./lib/rules/no-assigning-return-values'),
     'no-unnecessary-waiting': require('./lib/rules/no-unnecessary-waiting'),
     'assertion-before-screenshot': require('./lib/rules/assertion-before-screenshot'),
+    'require-data-selectors': require('./lib/rules/require-data-selectors'),
   },
   configs: {
     recommended: require('./lib/config/recommended'),
---
+++ tests/lib/rules/require-data-selectors.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const rule = require('../../../lib/rules/require-data-selectors')
+const RuleTester = require('eslint').RuleTester
+
+const ruleTester = new RuleTester()
+
+const errors = [{ messageId: 'unexpected' }]
+const parserOptions = { ecmaVersion: 6 }
+
+ruleTester.run('no-dynamic-id-classes', rule, {
+  valid: [
+    { code: 'cy.get(\'[data-cy=submit]\').click()', parserOptions },
+    { code: 'cy.get(\'[data-QA=submit]\')', parserOptions },
+    { code: 'cy.clock(5000)', parserOptions },
+    { code: 'cy.scrollTo(0, 10)', parserOptions },
+    { code: 'cy.tick(500)', parserOptions },
+  ],
+
+  invalid: [
+    { code: 'cy.get(\'[daedta-cy=submit]\').click()', parserOptions, errors },
+    { code: 'cy.get(\'[d-cy=submit]\')', parserOptions, errors },
+    { code: 'cy.get(".btn-large").click()', parserOptions, errors },
+    { code: 'cy.get(".btn-.large").click()', parserOptions, errors },
+    { code: 'cy.get(".a")', parserOptions, errors },
+  ],
+})
