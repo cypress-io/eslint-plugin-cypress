c2b1584b8c9091e506890564665af2432dd25187
Dominic Fraser <dominic.fraser@skyscanner.net>
2023-09-15 18:00:04 +0100

    add test to ensure 'focus' does fail, in addition to 'focused' passing

--- tests/lib/rules/unsafe-to-chain-command.js
+++ tests/lib/rules/unsafe-to-chain-command.js
@@ -32,6 +32,11 @@ ruleTester.run('action-ends-chain', rule, {
       errors,
     },
     {
+      code: 'cy.get("new-todo").focus().should("have.class", "active");',
+      parserOptions,
+      errors,
+    },
+    {
       code: 'cy.get("new-todo").customType("todo A{enter}").customClick();',
       parserOptions,
       errors,
