a36011d93635d320b5902914ae8ed4c7b9d6369e
Benoit Durand <benoit.durand.mail@gmail.com>
2025-08-14 11:11:54 +0200

    fix: dont throw warning on variable data selector

--- tests/lib/rules/require-data-selectors.js
+++ tests/lib/rules/require-data-selectors.js
@@ -17,6 +17,8 @@ ruleTester.run('require-data-selectors', rule, {
     { code: 'cy.get(\`[data-cy=${1}]\`)' }, // eslint-disable-line no-useless-escape
     { code: 'cy.get("@my-alias")' },
     { code: 'cy.get(`@my-alias`)' },
+    { code: 'const ASSESSMENT_SUBMIT = "[data-cy=assessment-submit]"; cy.get(ASSESSMENT_SUBMIT)' },
+    { code: 'const ALIAS_TEMPLATE = `@my-alias`; cy.get(ALIAS_TEMPLATE)' },
   ],
 
   invalid: [
@@ -26,5 +28,7 @@ ruleTester.run('require-data-selectors', rule, {
     { code: 'cy.get(".btn-.large").click()', errors },
     { code: 'cy.get(".a")', errors },
     { code: 'cy.get(\`[daedta-cy=${1}]\`)', errors }, // eslint-disable-line no-useless-escape
+    { code: 'const BAD_SELECTOR = ".my-class"; cy.get(BAD_SELECTOR)', errors },
+    { code: 'const GOOD = "[data-cy=good]"; const BAD = ".bad"; cy.get(GOOD); cy.get(BAD)', errors },
   ],
 })
