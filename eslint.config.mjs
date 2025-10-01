8b96bfb61258e3df3ec3967d0d931ba03927f5c2
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-10-01 16:43:32 +0200

    chore: lint TypeScript declaration files d.ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import js from '@eslint/js'
 import eslintPlugin from 'eslint-plugin-eslint-plugin'
 import mochaPlugin from 'eslint-plugin-mocha'
 import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
 
@@ -12,7 +13,7 @@ export default defineConfig([
   {
     files: ['**/*.{,m}js'],
     extends: [
-      pluginJs.configs.recommended,
+      js.configs.recommended,
       eslintPlugin.configs.recommended,
       mochaPlugin.configs.recommended,
       stylistic.configs.recommended,
@@ -35,5 +36,14 @@ export default defineConfig([
     languageOptions: {
       globals: globals.node,
     },
+  },
+  {
+    files: ['**/*.d.ts'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      stylistic.configs.recommended,
+    ],
   },
 ])
