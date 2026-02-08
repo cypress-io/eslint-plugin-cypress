9e0c2475d0b5749779c6cfbd4c38a3b2720236c2
Benoit Durand <benoit.durand.mail@gmail.com>
2026-02-08 20:27:44 +0100

    fix(require-tags): enhance test cases for tag requirements

--- tests/lib/rules/require-tags.js
+++ tests/lib/rules/require-tags.js
@@ -7,8 +7,12 @@ const ruleTester = new RuleTester()
 ruleTester.run('require-tags', rule, {
   valid: [
     { code: 'it(\'test\', { tags: [\'smoke\'] }, () => { cy.get(\'button\').click(); })' },
-
-    { code: 'it(\'works as an array\', { tags: [\'config\', \'some-other-tag\'] }, () => { expect(true).to.be.true })' },
+    { code: 'it(\'test\', { tags: [\'config\', \'some-other-tag\'] }, () => { expect(true).to.be.true })' },
+    { code: 'specify(\'test\', { tags: [\'smoke\'] }, () => { cy.get(\'button\').click(); })' },
+    { code: 'it.only(\'test\', { tags: [\'smoke\'] }, () => {})' },
+    { code: 'it.skip(\'test\', { tags: [\'smoke\'] }, () => {})' },
+    { code: 'specify.only(\'test\', { tags: [\'smoke\'] }, () => {})' },
+    { code: 'specify.skip(\'test\', { tags: [\'smoke\'] }, () => {})' },
   ],
 
   invalid: [
@@ -19,6 +23,26 @@ ruleTester.run('require-tags', rule, {
     {
       code: 'it(\'test\', { tags: [] }, () => { cy.get(\'button\').click(); })',
       errors: [{ messageId: 'emptyTags' }],
+    },
+    {
+      code: 'specify(\'test\', () => { expect(true).to.be.true })',
+      errors: [{ messageId: 'missingTags' }],
+    },
+    {
+      code: 'it.only(\'test\', () => {})',
+      errors: [{ messageId: 'missingTags' }],
+    },
+    {
+      code: 'it.skip(\'test\', () => {})',
+      errors: [{ messageId: 'missingTags' }],
+    },
+    {
+      code: 'specify.only(\'test\', () => {})',
+      errors: [{ messageId: 'missingTags' }],
+    },
+    {
+      code: 'specify.skip(\'test\', () => {})',
+      errors: [{ messageId: 'missingTags' }],
     },
   ],
 })
