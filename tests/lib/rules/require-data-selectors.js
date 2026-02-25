3a2e0859d43a806436f283a55653dca247ef6aed
Tim Oelkers <tim.oelkers@mgm-tp.com>
2026-02-25 13:04:36 +0100

    feat: require-data-selector uses consistent rules with assignments

--- tests/lib/rules/require-data-selectors.js
+++ tests/lib/rules/require-data-selectors.js
@@ -14,11 +14,13 @@ ruleTester.run('require-data-selectors', rule, {
     { code: 'cy.clock(5000)' },
     { code: 'cy.scrollTo(0, 10)' },
     { code: 'cy.tick(500)' },
-    { code: 'cy.get(\`[data-cy=${1}]\`)' }, // eslint-disable-line no-useless-escape
+    { code: 'cy.get(`[data-cy=${1}]`)' },
     { code: 'cy.get("@my-alias")' },
     { code: 'cy.get(`@my-alias`)' },
     { code: 'const ASSESSMENT_SUBMIT = "[data-cy=assessment-submit]"; cy.get(ASSESSMENT_SUBMIT)' },
     { code: 'const ALIAS_TEMPLATE = `@my-alias`; cy.get(ALIAS_TEMPLATE)' },
+    { code: 'const TEMPLATE = `[data-cy=${1}]`; cy.get(TEMPLATE)' },
+    { code: 'const ALIAS_TEMPLATE = `@my-alias`; const REASSIGNED = ALIAS_TEMPLATE; cy.get(REASSIGNED)' },
   ],
 
   invalid: [
@@ -27,8 +29,10 @@ ruleTester.run('require-data-selectors', rule, {
     { code: 'cy.get(".btn-large").click()', errors },
     { code: 'cy.get(".btn-.large").click()', errors },
     { code: 'cy.get(".a")', errors },
-    { code: 'cy.get(\`[daedta-cy=${1}]\`)', errors }, // eslint-disable-line no-useless-escape
+    { code: 'cy.get(`[daedta-cy=${1}]`)', errors },
     { code: 'const BAD_SELECTOR = ".my-class"; cy.get(BAD_SELECTOR)', errors },
     { code: 'const GOOD = "[data-cy=good]"; const BAD = ".bad"; cy.get(GOOD); cy.get(BAD)', errors },
+    { code: 'const TEMPLATE = `[daedta-cy=${1}]`; cy.get(TEMPLATE)', errors },
+    { code: 'const BAD = ".bad"; const REASSIGNED = BAD; cy.get(REASSIGNED)', errors },
   ],
 })
